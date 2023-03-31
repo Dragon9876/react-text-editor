bed6d809338acf293b9138fc15dab3ef877eb8b0
Dragon9876 <63802533+Dragon9876@users.noreply.github.com>
2023-03-31 23:56:22 +0300

    Delete src directory

--- src/components/common/BaseButton/BaseButton.tsx
+++
@@ -1,7 +0,0 @@
-import { ButtonHTMLAttributes, FC } from 'react';
-
-type BaseButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
-
-export const BaseButton: FC<BaseButtonProps> = ({ children, ...restProps }) => {
-   return <button {...restProps}>{children}</button>;
-};
--- src/components/common/BaseSvgSelect/types.in.ts
+++
@@ -1,5 +0,0 @@
-export enum SvgNames {
-   BOLD = 'bold',
-   ITALIC = 'italic',
-   UNDERLINE = 'underline'
-}
