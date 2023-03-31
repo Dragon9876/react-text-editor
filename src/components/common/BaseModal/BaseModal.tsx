09540c69f3a4fe7c2e77fb159c31e794208b5ed8
romanmalanchuk <roman.malanchuk212@gmail.com>
2023-03-31 23:02:40 +0300

    fix some errors

--- src/components/common/BaseModal/BaseModal.tsx
+++ src/components/common/BaseModal/BaseModal.tsx
@@ -8,15 +8,13 @@ interface BaseModalProps {
    isModalOpen: boolean;
 }
 
-const app = document.getElementById('root');
-
 export const BaseModal: FC<BaseModalProps> = ({ children, isModalOpen }) => {
    return isModalOpen
       ? createPortal(
            <div className={styles.modal}>
               <div className={styles.modal__inner}>{children}</div>
            </div>,
-           app
+           document.getElementById('root')!
         )
       : null;
 };
